67d63fa2c32a2c530af10cebcefa40f1d35ec764
DuranPX <australiaparacall@gmail.com>
2026-03-07 13:34:21 -0500

    implementacion rutas LAN, def de puerto y optimizacion de App.tsx + AppRouter.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { Login } from "./pages/Login"
-import { Register } from "./pages/Register"
-import { Dashboard } from "./pages/Dashboard"
-import { PrivateRoute } from "./routes/PrivateRoute"
+import AppRouter from "./router/AppRouter"
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-
-        <Route path="/register" element={<Register />} />
-
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  )
+  return <AppRouter />
 }
 
 export default App
